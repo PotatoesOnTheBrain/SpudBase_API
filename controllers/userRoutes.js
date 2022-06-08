30f2aba8086f613160fb5fd157a48c45508220cb
Danny Kwolek <daniel.m.kwolek@gmail.com>
2022-06-08 16:42:31 -0700

    Finished implementation of user/userinfo post endpoint

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -3,20 +3,38 @@ const router = express.Router();
 const axios = require("axios");
 const Session = require('../models/Session')
 
-router.post('/userinfo',(req,res)=>{
-Session.find({session_id: req.body.session_id})
-.then(session =>{
-axios.get("https://api.github.com/user", {headers:{Accept: "application/json", Authorization: `token ${session.access_token}`}})
-.then(response =>{
-    console.log(response)
-    res.send('working on it')
-})
+router.post('/userinfo',(req,res)=> {
+    console.log(req.body.session_id)
+
+    Session.findOne({session_id: req.body.session_id})
+    .then(session => {
+        console.log("session = ", session)
+        console.log("session.access_token = ", session.access_token);
+        return axios.get("https://api.github.com/user", {headers:{Accept: "application/json", Authorization: `token ${session.access_token}`}})
+    })
+    .then(response => {
+        console.log("response.data =", response.data)
+        res.json(response.data)
+    })
+    .catch(error => {
+        console.log("error =", error)
+        res.send(error)
+    })
 })
 
-.catch(error => {
-    res.send(error)
-})
-})
+
+/*
+promiseReturningFunction()
+    .then(() => {AnotherPromiseReturningFunction()}) ON SUCCESS DO SOMETHING
+    .then(() => {}) ON SUCCESS OF AnotherPromiseReturningFunction()
+    .catch() ON FAILURE DO SOMETHING ELSE
+*/
 
 
 module.exports=router;
+
+// axios.get("https://api.github.com/user", {headers:{Accept: "application/json", Authorization: `token ${session.access_token}`}})
+// .then(response =>{
+//     console.log(response)
+//     res.send('working on it')
+// })
