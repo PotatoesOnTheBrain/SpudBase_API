f84ea0c70f4f5378a840bc6cdce407cb97ed89fe
Andre Booth <DreLeRoy@MacBook-Pro.local>
2022-06-08 15:31:36 -0700

    created userInfoEndpoint

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const Session = require('../models/Session')
 
-router.get('/userinfo',(req,res)=>{
-res.send('some info')
+router.post('/userinfo',(req,res)=>{
+Session.find({session_id: req.body.session_id})
+.then(session =>{
+axios.get("https://api.github.com/user", {headers:{Accept: "application/json", Authorization: `token ${session.access_token}`}})
+.then(response =>{
+    console.log(response)
+    res.send('working on it')
+})
+})
 
+.catch(error => {
+    res.send(error)
+})
 })
 
 
