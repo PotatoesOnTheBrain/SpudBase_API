583498543827a7bb282301b8174a3f45107870bc
Gabrielle Walsh <gabmwalsh@gmail.com>
2022-07-01 17:08:53 -0400

    add mobile auth route

--- controllers/oAuthRoutes.js
+++ controllers/oAuthRoutes.js
@@ -37,9 +37,48 @@ router.get("/authorize", (req, res) => {
                 console.log("failed to associate session_id with user_id")
                 return;
             }
-            session.user_id = response.data.login.toLowerCase();
+            session.user_id = response.data.login;
             session.save();
-            console.log(response.data.login.toLowerCase());
+        })
+        .catch(error => {
+            console.log(error)
+            res.json(error)
+        })
+})
+
+router.get("/authorizemobile", (req, res) => {
+    let session = {};
+    axios.get(`https://github.com/login/oauth/access_token?code=${req.query.code}&client_id=${process.env.MOBILE_ID}&client_secret=${process.env.MOBILE_SECRET}`, {headers: {Accept: "application/json"}})
+        .then(ires => {
+            if(!ires.data) {
+                return Promise.reject({error: "failure to fetch response data from github api"})
+            }
+            if (ires.data.error) {
+                return Promise.reject({error: ires.data.error});
+            }else {
+                let session_id = uuidv4();
+                return (Session.create({
+                    session_id: session_id,
+                    access_token: ires.data.access_token
+                }))
+            }
+        })
+        .then(newSession => {
+            if (!newSession) {
+                return Promise.reject({error: "Failure to create new session, please clear cookies and try again"})
+            }
+            session = newSession
+            res.json({session_id: session.session_id})
+            return axios.get("https://api.github.com/user", {headers:{Accept: "application/json", Authorization: `token ${session.access_token}`}})
+        })
+        .then(response => {
+            //TODO handle failure to get userdata after session creation
+            if (!response) {
+                console.log("failed to associate session_id with user_id")
+                return;
+            }
+            session.user_id = response.data.login;
+            session.save();
         })
         .catch(error => {
             console.log(error)
--- models/Session.js
+++ models/Session.js
@@ -3,7 +3,8 @@ const mongoose = require("../connection.js");
 const SessionSchema = new mongoose.Schema({
     session_id: String,
     access_token: String,
-    user_id: String
+    user_id: String,
+    user_name: String
 })
 
 module.exports = mongoose.model("Session", SessionSchema);
